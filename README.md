d70664cd5fc6be1c4c2169387e709957a8f54cd3
Sarah Ohlin <sarahohlin1@gmail.com>
2019-12-16 13:24:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mvc-test
+
+Testing the development of an mvc in express / ts
+
+## Configuring the dev environment
---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get update
+
+# setup mongo-db
+
+apt install -y mongodb
+systemctl enable --now mongodb
+
+# install node
+
+apt install -y nodejs
+apt install -y npm
+
+# set up the hot reloader
+
+cd /home/vagrant/workspace
+npm install
+npm run dev &
